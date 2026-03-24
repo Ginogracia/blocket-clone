9dd2a0798bbae1a8a1e4b9a1ffc5700e4e4b72b5
Ginogracia <gino.gracia@hotmail.com>
2026-03-24 16:54:50 +0100

    Adds register and login pages with functioning auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <main className="main-content">
         <Routes>
           <Route path="/" element={<div>Home</div>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </main>
     </BrowserRouter>
